705568ed3630ca454e7988c542d891654a77d9e7
iki1uc <entertrainer@web.de>
2026-08-18 13:46:48 +0200

    Create ufo.seti.js

---
+++ ufo.seti.js
@@ -0,0 +1,10 @@
+// ufo.seti.js — SETI Analyse
+
+export function UFO_SETI(data){
+    return {
+        keplerDeviation: data.qi - data.iqq,
+        resonance: data.tiiummm,
+        axisDrift: data.quuee,
+        anomaly: data.anomaly
+    };
+}
